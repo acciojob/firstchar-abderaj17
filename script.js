0233c98c72c2a82b79a92d8c2137c69e7e115cad
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-12-20 16:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function firstChar(text) {
   // your code here
-}
+	let trimmedString = text.trim();
+		if(trimmedString === ''){
+	        return '';		
+		}else
+		{
+		return	trimmedString[0];
+		}
+	}
+
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
